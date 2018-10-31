d1deb4f35ad274eb6912b03f4ff0d1af148adcb6
Davecodes <davidcreador@gmail.com>
2018-10-31 16:39:28 -0600

    using latest react and react-hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { spring, AnimatedSwitch } from 'react-router-transition'
 import 'bulma/css/bulma.css'
@@ -31,51 +31,45 @@ function bounce(val) {
   })
 }
 
-class App extends Component {
-  state = {
-    expanded: false
-  }
+export default () => {
+  const [expanded, setExpanded] = useState(false)
 
-  render() {
-    const bounceTransition = {
-      // start in a transparent, upscaled state
-      atEnter: {
-        opacity: 0,
-        scale: 1.2
-      },
-      // leave in a transparent, downscaled state
-      atLeave: {
-        opacity: bounce(0),
-        scale: bounce(0.8)
-      },
-      // and rest at an opaque, normally-scaled state
-      atActive: {
-        opacity: bounce(1),
-        scale: bounce(1)
-      }
+  const bounceTransition = {
+    // start in a transparent, upscaled state
+    atEnter: {
+      opacity: 0,
+      scale: 1.2
+    },
+    // leave in a transparent, downscaled state
+    atLeave: {
+      opacity: bounce(0),
+      scale: bounce(0.8)
+    },
+    // and rest at an opaque, normally-scaled state
+    atActive: {
+      opacity: bounce(1),
+      scale: bounce(1)
     }
-
-    return (
-      <div className={this.state.expanded ? 'main-wrapper fullscreen' : 'main-wrapper'}>
-        <NavBar expanded={this.state.expanded} />
-        <Switch>
-          <AnimatedSwitch
-            atEnter={bounceTransition.atEnter}
-            atLeave={bounceTransition.atLeave}
-            atActive={bounceTransition.atActive}
-            mapStyles={mapStyles}
-            className="switch-wrapper"
-          >
-            <Route exact path="/" component={FirstPage} />
-            <Route exact path="/about" component={AboutPage} />
-            <Route exact path="/projects" component={ProjectsPage} />
-            <Route exact path="/contact" component={ContactPage} />
-            <Route component={FirstPage} />
-          </AnimatedSwitch>
-        </Switch>
-      </div>
-    )
   }
-}
 
-export default App
+  return (
+    <div className={expanded ? 'main-wrapper fullscreen' : 'main-wrapper'}>
+      <NavBar expanded={expanded} />
+      <Switch>
+        <AnimatedSwitch
+          atEnter={bounceTransition.atEnter}
+          atLeave={bounceTransition.atLeave}
+          atActive={bounceTransition.atActive}
+          mapStyles={mapStyles}
+          className="switch-wrapper"
+        >
+          <Route exact path="/" component={FirstPage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/projects" component={ProjectsPage} />
+          <Route exact path="/contact" component={ContactPage} />
+          <Route component={FirstPage} />
+        </AnimatedSwitch>
+      </Switch>
+    </div>
+  )
+}
